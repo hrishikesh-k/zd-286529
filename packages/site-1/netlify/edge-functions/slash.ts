a8c2a817144cdb372a307afb4aab243c27fbbb61
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-09-05 18:24:32 +0000

    enable the ef

--- packages/site-1/netlify/edge-functions/slash.ts
+++ packages/site-1/netlify/edge-functions/slash.ts
@@ -1,6 +1,17 @@
+import type {Config} from '@netlify/edge-functions'
+
 export default async function(req: Request) {
   const url = new URL(req.url)
   if (url.pathname.endsWith('/')) {
     return new URL(url.pathname.slice(0, -1), req.url)
   }
+}
+
+export const config: Config = {
+  excludedPath: [
+    '/favicon.ico',
+    '/*.css',
+    '/.js'
+  ],
+  path: '/*'
 }
